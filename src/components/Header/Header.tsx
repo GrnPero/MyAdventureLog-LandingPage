9bfe8f09a6ced23fd8f1d05f5cee9fba1b619984
Rodrigo Alvarez <r.alvarezcobarruvias@gmail.com>
2024-03-01 15:56:54 -0800

    Finished desktop version of header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,25 +2,32 @@ import React from "react";
 import NavBar from "../NavBar/NavBar";
 import * as styles from "./Header.module.css";
 import GooglePlay from "../../../static/images/Google_Play-Badge-Logo.svg";
-import IPhone from "../../../static/images/iPhone.png";
+import IPhone from "../../../static/images/iphone_desktop.png";
 
 const Header = () => {
   return (
     <div className={styles.background}>
       <NavBar />
-      <h1>
-        Welcome <div className={styles.header__special}>to</div> MyAdventureLog
-      </h1>
-      <p>Your Personal Journey Tracker</p>
-      <div className={styles.container}>
-        <img
-          src={GooglePlay}
-          alt="Google Play Logo"
-          className={styles.googleplay}
-        />
-      </div>
-      <div className={styles.container}>
-        <img src={IPhone} alt="IPhone" className={styles.iphone} />
+      <div className={styles.grid}>
+        <div className={styles.header__hero}>
+          <h1>
+            Welcome <div className={styles.header__special}>to</div>{" "}
+            MyAdventureLog
+          </h1>
+          <p>Your Personal Journey Tracker</p>
+          <div className={styles.container}>
+            <img
+              src={GooglePlay}
+              alt="Google Play Logo"
+              className={styles.googleplay}
+            />
+          </div>
+        </div>
+        <div className={styles.header__iphone}>
+          <div className={styles.container}>
+            <img src={IPhone} alt="IPhone" className={styles.iphone} />
+          </div>
+        </div>
       </div>
     </div>
   );
