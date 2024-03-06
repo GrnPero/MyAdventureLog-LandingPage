321b304d2e7d0bdfb34258cfbcec9081cd6a7b5f
Rodrigo Alvarez <r.alvarezcobarruvias@gmail.com>
2024-03-06 01:58:48 -0800

    Finished introduction section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,48 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
 import Layout from "../components/layout";
-import Header from "../components/Header/Header";
+import * as introductionStyles from "../components/Introduction/Introduction.module.css";
+import GooglePlay from "../../static/images/Google_Play-Badge-Logo.svg";
+import iPhone from "../../static/images/IntroductioniPhone.png";
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout>
-      <h1>Heading 1</h1>
-      <p>Test</p>
+      <section
+        className={`${introductionStyles.container}
+          ${introductionStyles.grid}
+          ${introductionStyles.block}
+          ${introductionStyles.gridOneTwo}`}
+      >
+        <div className={introductionStyles.headerContainer}>
+          <h1 className={introductionStyles.altHeading}>
+            Embark on Your Personal Adventure with MyAdventureLog
+          </h1>
+          <p className={introductionStyles.headingText}>
+            Welcome to MyAdventureLog, the ultimate companion for capturing and
+            reliving your most memorable moments. Our app is designed to empower
+            you to chronicle your journeys, big or small, with ease and style.
+          </p>
+          <div
+            className={`${introductionStyles.imageContainer} ${introductionStyles.googlePlayContainer}`}
+          >
+            <img
+              src={GooglePlay}
+              alt="Get it on the Google Play store"
+              className={introductionStyles.playStore}
+            />
+          </div>
+        </div>
+        <div
+          className={`${introductionStyles.imageContainer} ${introductionStyles.imageOrder}`}
+        >
+          <img
+            src={iPhone}
+            alt="iPhone running the app"
+            className={introductionStyles.iphone}
+          />
+        </div>
+      </section>
     </Layout>
   );
 };
