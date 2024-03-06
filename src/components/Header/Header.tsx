522d6fe46b07ff01a2a5206f2c8cf530c83c4e91
Rodrigo Alvarez <r.alvarezcobarruvias@gmail.com>
2024-03-05 23:34:01 -0800

    Fixed styling for header components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,24 +8,26 @@ const Header = () => {
   return (
     <div className={styles.background}>
       <NavBar />
-      <div className={styles.grid}>
-        <div className={styles.header__hero}>
-          <h1>
-            Welcome <div className={styles.header__special}>to</div>{" "}
-            MyAdventureLog
-          </h1>
-          <p>Your Personal Journey Tracker</p>
-          <div className={styles.container}>
-            <img
-              src={GooglePlay}
-              alt="Google Play Logo"
-              className={styles.googleplay}
-            />
+      <div className={styles.container}>
+        <div className={styles.grid}>
+          <div className={styles.header__hero}>
+            <h1>
+              Welcome <div className={styles.header__special}>to</div>{" "}
+              MyAdventureLog
+            </h1>
+            <p>Your Personal Journey Tracker</p>
+            <div className={styles.containerImage}>
+              <img
+                src={GooglePlay}
+                alt="Google Play Logo"
+                className={styles.googleplay}
+              />
+            </div>
           </div>
-        </div>
-        <div className={styles.header__iphone}>
-          <div className={styles.container}>
-            <img src={IPhone} alt="IPhone" className={styles.iphone} />
+          <div className={styles.header__iphone}>
+            <div className={styles.containerImage}>
+              <img src={IPhone} alt="IPhone" className={styles.iphone} />
+            </div>
           </div>
         </div>
       </div>
