af5624cdf1b3e9439cd0fd5a91a340f16f5d12b6
Rodrigo Alvarez <r.alvarezcobarruvias@gmail.com>
2024-03-27 14:03:27 -0700

    Finished

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `MyAdventureLog-LandingPage`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://myadventurelog.app`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
